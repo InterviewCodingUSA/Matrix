941352499a93cfbe0ba09b1a1d4c5669c4d1eb03
Ashish <ashishsingh@gmail.com>
2025-08-16 17:24:07 -0700

    Checking in code for Word search in a matrix.

---
+++ WordSearch/WordSearch/src/BoxValue.java
@@ -0,0 +1,7 @@
+public class BoxValue <T>{
+    public T value;
+    public BoxValue(T value){
+        this.value = value;
+    }
+
+}
---
+++ WordSearch/WordSearch/src/Main.java
@@ -0,0 +1,58 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        String[][] matrix = {
+                {"A","B","C","E"},
+                {"S","F","C","S"},
+                {"A","D","E","E"}
+        };
+        System.out.println( searchWordInMatrix("ABCCE", matrix) );
+    }
+
+    private static boolean searchWordInMatrix(String word, String[][] matrix){
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        boolean[][] visited = new boolean[rows][cols];
+        BoxValue<Boolean> box = new BoxValue<>(false);
+        for(int i = 0 ; i < rows; i ++){
+            for(int j = 0 ; j < cols; j ++){
+                visitNeighbours(matrix, i,j, visited, word, 0, box);
+                if(box.value == true){
+                    System.out.println("Found the word " + word);
+                    return true;
+                }
+            }
+        }
+        return box.value;
+
+    }
+    private static void visitNeighbours(String[][] matrix, int row, int col , boolean[][] visited, String word, int index,
+                                        BoxValue<Boolean> box){
+        String charAtIndex =  Character.toString(word.charAt(index));
+        if(row < 0 || col < 0 || row >= matrix.length || col >= matrix[0].length ||
+                !matrix[row][col].equalsIgnoreCase(charAtIndex)
+                || visited[row][col] == true){
+            return ;
+        }
+        // this is the last character and we have matched
+        if(matrix[row][col].equalsIgnoreCase(charAtIndex) && index == word.length() -1){
+            box.value = true;
+            return ;
+        }
+        visited[row][col] = true;
+        visitNeighbours(matrix, row-1, col, visited, word, index+1, box); // top neighbour
+        visitNeighbours(matrix, row +1, col, visited, word, index+1, box); // bottom neighbour
+        visitNeighbours(matrix, row, col -1 , visited, word, index+1, box); // left neighbour
+        visitNeighbours(matrix, row, col + 1 , visited, word, index+1, box); // right neighbour
+
+//        // If we want to consider diagonals too then uncomment following code.
+//        visitNeighbours(matrix, row-1, col-1, visited, word, index+1, box); // top left diagonal  neighbour
+//        visitNeighbours(matrix, row - 1, col + 1, visited, word, index+1, box); // top right diagonal  neighbour
+//        visitNeighbours(matrix, row + 1, col -1 , visited, word, index+1, box); // bottom left diagonal  neighbour
+//        visitNeighbours(matrix, row + 1, col + 1 , visited, word, index+1, box); // bottom right diagonal  neighbour
+
+    }
+
+}
